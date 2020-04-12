8b8aee05b915d25fcb5be0132f41ca81f5859b30
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-04-12 14:53:02 -0700

    First commit 04/12/2020

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+const temp = (clickedButton, divID) => {
+    setTimeout(() => {
+        var item = document.getElementById(divID);
+        if (item) {
+            if(item.className=='btn btn-secondary hidden'){
+                item.className = 'btn btn-secondary unhidden' ;
+                clickedButton.value = 'hide'
+            }else{
+                item.className = 'btn btn-secondary hidden';
+                clickedButton.value = 'unhide'
+            }
+        }
+    }, 1000)
+}
