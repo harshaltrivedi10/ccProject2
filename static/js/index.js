8080cdb2a4e05bc628f4b4a42a8e6b2fcba5970d
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-04-27 23:29:57 -0700

    First gcloud push ready

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,14 @@
-const temp = (clickedButton, divID) => {
+const submitRequest = (clickedButton, divID) => {
     setTimeout(() => {
         var item = document.getElementById(divID);
         if (item) {
             if(item.className=='btn btn-secondary hidden'){
                 item.className = 'btn btn-secondary unhidden' ;
-                clickedButton.value = 'hide'
+                clickedButton.value = 'hide';
             }else{
                 item.className = 'btn btn-secondary hidden';
-                clickedButton.value = 'unhide'
+                clickedButton.value = 'unhide';
             }
         }
-    }, 1000)
+    }, 1000);
 }
